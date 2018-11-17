e23d84434b55533444c8d52eea50264686437e04
Clara Devers <clara.j.devers@gmail.com>
2018-11-17 16:15:33 -0500

    add complete goal boilerplate text

--- src/handlers/CompleteGoal.js
+++ src/handlers/CompleteGoal.js
@@ -1,16 +1,16 @@
 /**
- * Handler for `AMAZON.HelpIntent` requests
+ * Handler for `CompleteGoal` requests
  */
 module.exports = {
     canHandle({ requestEnvelope }) {
       const {
         request: { type, intent },
       } = requestEnvelope;
-      return type === 'IntentRequest' && intent.name === 'AMAZON.HelpIntent';
+      return type === 'IntentRequest' && intent.name === 'CompleteGoal';
     },
   
     handle({ responseBuilder }) {
-      const output = 'What can I help you with?';
+      const output = 'Yas! You completed your goal.';
       return responseBuilder
         .speak(output)
         .reprompt(output)
