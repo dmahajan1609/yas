cef806fe12c763bfa8f2dae67204f2f71012b0bd
Mahajan, Divya (01S) <divya.mahajan@fmr.com>
2018-11-17 16:56:50 -0500

    adding createGoal

--- src/handlers/CreateGoal.js
+++ src/handlers/CreateGoal.js
@@ -6,11 +6,11 @@ module.exports = {
       const {
         request: { type, intent },
       } = requestEnvelope;
-      return type === 'IntentRequest' && intent.name === 'AMAZON.HelpIntent';
+      return type === 'IntentRequest' && intent.name === 'CreateGoal';
     },
   
     handle({ responseBuilder }) {
-      const output = 'What can I help you with?';
+      const output = 'Yas! We created a goal to complete the hackathon. Let\'s get started';
       return responseBuilder
         .speak(output)
         .reprompt(output)
