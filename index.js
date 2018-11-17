a9c91cfd647dbf8e4466683591f2eda051baafc6
Mahajan, Divya (01S) <divya.mahajan@fmr.com>
2018-11-17 15:37:33 -0500

    adding working launchRequest

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const Alexa = require('ask-sdk-core');
-const {
-  CancelStopHandler,
-  ErrorHandler,
-  FallbackHandler,
-  HelpHandler,
-  SessionEndedHandler,
-  LaunchRequestHandler,
-  // GoalHandler,
-  // StepsHandler,
-  // TasksHandler,
-} = require('./src/handlers');
+const CancelStopHandler = require('./src/handlers/AmazonCancelStop');
+const ErrorHandler = require('./src/handlers/Error');
+const FallbackHandler = require('./src/handlers/AmazonFallback');
+const HelpHandler = require('./src/handlers/AmazonHelp');
+const SessionEndedHandler = require('./src/handlers/SessionEnded');
+const LaunchRequestHandler = require('./src/handlers/LaunchRequest');
 
 /* Replace with skill id from Alexa developer console */
 const alexAppId = 'amzn1.ask.skill.54c76e12-9b20-47ac-a939-e1f5503d8bb9';
